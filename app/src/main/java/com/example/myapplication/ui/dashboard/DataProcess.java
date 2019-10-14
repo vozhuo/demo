5432cfcc522410d45e50dc0bd60aa906d35dc886
vozhuo <qsx753698@outlook.com>
2019-10-14 10:04:03 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/ui/dashboard/DataProcess.java
@@ -0,0 +1,67 @@
+package com.example.myapplication.ui.dashboard;
+
+import com.alicloud.openservices.tablestore.SyncClient;
+import com.alicloud.openservices.tablestore.model.*;
+
+public class DataProcess {
+    private static final String endPoint = "https://data-station.cn-hangzhou.ots.aliyuncs.com";
+    private static final String accessKeyId = "LTAI4Fr2MVED6cBoyWpuerQK";
+    private static final String accessKeySecret = "WsebpOCKI3IKODJtZsvD5nVLQXxN9S";
+    private static String instanceName = "data-station";
+
+    private static final String PRIMARY_KEY_NAME = "time";
+    private static final String PRIMARY_KEY_VALUE = "device1";
+    private static final String TABLE_NAME = "station";
+
+    public static String getRow() {
+
+        SyncClient client = new SyncClient(endPoint, accessKeyId, accessKeySecret, instanceName);
+
+        // 构造主键
+        PrimaryKeyBuilder primaryKeyBuilder = PrimaryKeyBuilder.createPrimaryKeyBuilder();
+        primaryKeyBuilder.addPrimaryKeyColumn(PRIMARY_KEY_NAME, PrimaryKeyValue.fromString(PRIMARY_KEY_VALUE));
+        PrimaryKey primaryKey = primaryKeyBuilder.build();
+
+        // 读一行
+        SingleRowQueryCriteria criteria = new SingleRowQueryCriteria(TABLE_NAME, primaryKey);
+        // 设置读取最新版本
+        criteria.setMaxVersions(100);
+        GetRowResponse getRowResponse = client.getRow(new GetRowRequest(criteria));
+        Row row = getRowResponse.getRow();
+
+        System.out.println("读取完毕, 结果为: ");
+        System.out.println(row);
+        return String.valueOf(row);
+    }
+
+    public static void putRow() {
+        SyncClient client = new SyncClient(endPoint, accessKeyId, accessKeySecret, instanceName);
+
+        // 构造主键
+        PrimaryKeyBuilder primaryKeyBuilder = PrimaryKeyBuilder.createPrimaryKeyBuilder();
+        primaryKeyBuilder.addPrimaryKeyColumn(PRIMARY_KEY_NAME, PrimaryKeyValue.fromString(PRIMARY_KEY_VALUE));
+        PrimaryKey primaryKey = primaryKeyBuilder.build();
+
+        RowPutChange rowPutChange = new RowPutChange(TABLE_NAME, primaryKey);
+
+        //插入列
+        rowPutChange.addColumn(new Column("col2", ColumnValue.fromDouble(123.5)));
+
+        client.putRow(new PutRowRequest(rowPutChange));
+    }
+
+    public static void updateRow() {
+        SyncClient client = new SyncClient(endPoint, accessKeyId, accessKeySecret, instanceName);
+        // 构造主键
+        PrimaryKeyBuilder primaryKeyBuilder = PrimaryKeyBuilder.createPrimaryKeyBuilder();
+        primaryKeyBuilder.addPrimaryKeyColumn(PRIMARY_KEY_NAME, PrimaryKeyValue.fromString(PRIMARY_KEY_VALUE));
+        PrimaryKey primaryKey = primaryKeyBuilder.build();
+
+        RowUpdateChange rowUpdateChange = new RowUpdateChange(TABLE_NAME, primaryKey);
+
+        // 更新列
+        rowUpdateChange.put(new Column("level", ColumnValue.fromDouble(66.6)));
+
+        client.updateRow(new UpdateRowRequest(rowUpdateChange));
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/ui/home/HomeFragment.java
@@ -0,0 +1,86 @@
+package com.example.myapplication.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.AMapOptions;
+import com.amap.api.maps.MapView;
+import com.amap.api.maps.UiSettings;
+import com.amap.api.maps.model.CameraPosition;
+import com.amap.api.maps.model.LatLng;
+import com.amap.api.maps.model.Marker;
+import com.amap.api.maps.model.MarkerOptions;
+
+import com.example.myapplication.R;
+
+
+public class HomeFragment extends Fragment {
+    private MapView mMapView = null;
+    private HomeViewModel homeViewModel;
+
+
+    public static final LatLng SHANGHAI = new LatLng(31.238068, 121.501654);// 上海市经纬度
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        homeViewModel =
+                ViewModelProviders.of(this).get(HomeViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+
+        //获取地图控件引用
+        mMapView = root.findViewById(R.id.map);
+        //在activity执行onCreate时执行mMapView.onCreate(savedInstanceState)，创建地图
+        mMapView.onCreate(savedInstanceState);
+        //定义了一个地图view
+        AMap aMap = mMapView.getMap();
+
+
+        CameraPosition LUJIAZUI = new CameraPosition.Builder()
+                .target(SHANGHAI).zoom(18).bearing(0).tilt(30).build();
+
+        AMapOptions aOptions = new AMapOptions();
+        aOptions.tiltGesturesEnabled(false);// 禁止通过手势倾斜地图
+        aOptions.camera(LUJIAZUI);
+
+        return root;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView.onResume ()，重新绘制加载地图
+        mMapView.onResume();
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView.onPause ()，暂停地图的绘制
+        mMapView.onPause();
+    }
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        //在activity执行onSaveInstanceState时执行mMapView.onSaveInstanceState (outState)，保存地图当前的状态
+        mMapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onDestroy() {
+//        setCameraPosition(aMap.getCameraPosition());//保存地图状态
+        super.onDestroy();
+        mMapView.onDestroy();
+    }
+}
